78ab862e19425a662708703175e6171c1fc6e7b9
zidage <ziyurue@qq.com>
2024-03-21 15:44:32 +0800

    add map_parser and update university information

--- data_fetcher/osm_data_fetcher/university_location_fetch_osm.py
+++ data_fetcher/osm_data_fetcher/university_location_fetch_osm.py
@@ -6,6 +6,7 @@ import osmnx as ox
 import matplotlib.pyplot as plt
 import os
 
+
 google_api_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&language=zh-CN&key={API_KEY}'
 input_file_path = ['map_data/catagory/china_university.json', 'map_data/catagory/world_university.json']
 amenities = ["restaurant", "cafe", "library", "hospital",
@@ -23,20 +24,24 @@ for input_path in input_file_path:
 			csv_features_path = 'map_data/university_map/{query}/{query}_{feature}.csv'
 			
 			try:
+				if not os.path.exists(f'map_data/university_map/{query}'):
+					os.makedirs(f'map_data/university_map/{query_formal_name}')
+				else:
+					continue
 				response = requests.get(google_api_url.format(query=query, API_KEY='AIzaSyAa2ZdkUiY7jLWkZJABgUtKMwEdHQA1pvU'))
 				assert(response.status_code == 200)
 				result = response.json()['results'][0]
-				data = {'name': result['name'], 'address': result['formatted_address'], 'rating': result['rating']}
+				data = {'name': result['name'], 'address': result['formatted_address'], 
+						'formatted_address': result['plus_code']['compound_code'].replace(' ', '_'),
+						'rating': result['rating']}
 				data_with_parent = {'university': data}
-				if not os.path.exists(f'map_data/university_map/{query}'):
-					os.makedirs(f'map_data/university_map/{query_formal_name}')
 				with open(f'map_data/university_map/{query_formal_name}/{query_formal_name}_info.json', 'w', encoding='gb2312') as f:
 					json.dump(data_with_parent, f, indent=4, ensure_ascii=False)
 					print(f'Basic info of {query} has been found and stored at map_data/university_map/{query}/{query_formal_name}_info.json')
 			except:
-				time.sleep(1)
-				print('\n')
-				break
+				# time.sleep(1)
+				print(f"{query} does not exits\n")
+				continue
 			try:
 				graph = ox.graph_from_place(query)
 				nodes, edges = ox.graph_to_gdfs(graph)
@@ -66,4 +71,10 @@ for input_path in input_file_path:
 					print(f"{len(feature)} {amenity} in {query} have been found andstored at {csv_features_path.format(query=query_formal_name, feature=amenity)}")
 				except:
 					print(f"No {amenity} was found in {query}")
+			try:
+				water = ox.features_from_place(query, {"natural": "water"})
+				water.to_csv(csv_features_path.format(query=query_formal_name, feature='water'))
+				print(f"{len(water)} water area in {query} have been found and stored at {csv_features_path.format(query=query_formal_name, feature='water')}")
+			except:
+				print(f"No water was found in {query}")
 			print("\n")
--- map_data/data_parser/map_parser.py
+++ map_data/data_parser/map_parser.py
@@ -1,5 +1,10 @@
 import geopandas as gpd
 import os
+import shutil
+import matplotlib as plt
+
+
+
 
 def list_subdirectories(root_dir):
     subdirectories = []
@@ -12,9 +17,66 @@ def list_subdirectories(root_dir):
             # 递归地调用list_subdirectories函数，继续遍历子文件夹
     return subdirectories
 
-working_directory = 'map_data/university_map'
-university_directories = list_subdirectories(working_directory)
-print(len(university_directories))
+
+def delete_folders_with_few_files(root_dir, min_files=2):
+    for dirpath, dirnames, filenames in os.walk(root_dir, topdown=False):
+        if len(filenames) <= min_files and dirpath != root_dir:  # 排除根文件夹
+            for file in filenames:
+                os.remove(os.path.join(dirpath, file))  # 删除文件
+            os.rmdir(dirpath)  # 删除空文件夹
+            print(f"Deleted folder: {dirpath}")
+
+
+def get_files_in_folder(folder_path):
+    files = []
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isfile(file_path):
+            files.append(file_path)
+    return files
+
+
+def info_parser(file):
+    return
+
+
+def regular_parser(file):
+    return gpd.read_file(file, GEOM_POSSIBLE_NAMES="geometry", KEEP_GEOM_COLUMNS="NO")
+
+
+def amenity_parser(file):
+    return gpd.read_file(file, GEOM_POSSIBLE_NAMES="geometry", KEEP_GEOM_COLUMNS="NO")
+
+
+root_dir = 'map_data/university_map'
+delete_folders_with_few_files(root_dir)
+university_directories = list_subdirectories(root_dir)
+
+for university in university_directories:
+    files_path = get_files_in_folder(university)
+    map_basket = {"node": None, "edge": None, "area": None, 
+                  "building": None, "water": None, "amenity": []}
+    for file in files_path:   
+        parsed_line = file.split('_')
+        file_type = parsed_line[-1]
+        if file_type == 'info.json':
+            info_parser(file)
+        elif file_type == 'nodes.csv':
+            map_basket["node"] = regular_parser(file)
+        elif file_type == 'edges.csv':
+            map_basket["edge"] = regular_parser(file)
+        elif file_type == 'area.csv':
+            map_basket["area"] = regular_parser(file)
+        elif file_type == 'buildings.csv':
+            map_basket["building"] = regular_parser(file)
+        elif file_type == 'water.csv':
+            map_basket["water"] = regular_parser(file)
+        else:
+            map_basket["amenity"].append(amenity_parser(file))
+    print(map_basket)
+    print()
+        
+            
 
 
 
