88ea77abb69556d53e5f052088db79fbac68efed
zidage <ziyurue@qq.com>
2024-03-24 20:55:10 +0800

    Update university data, and add university map thumbnail.

--- data_fetcher/osm_data_fetcher/university_location_fetch_osm.py
+++ data_fetcher/osm_data_fetcher/university_location_fetch_osm.py
@@ -1,3 +1,14 @@
+"""
+地图数据获取模块, 获取数据源: OpenStreetMap
+功能: 使用osmnx库以及Google Map Platform中的Place API请求地点名称列表中各地点的详细数据
+
+作者: 字禹润
+
+创建日期: 2024-03-20
+最后修改日期: 2024-03-24
+"""
+
+
 import json
 from urllib import response
 import requests
@@ -7,12 +18,10 @@ import matplotlib.pyplot as plt
 import os
 
 
+# Google Map Platform API request URL
 google_api_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&language=zh-CN&key={API_KEY}'
 input_file_path = ['map_data/catagory/china_university.json', 'map_data/catagory/world_university.json']
-amenities = ["restaurant", "cafe", "library", "hospital",
-        	"cinema", "bar", "bus_station", 
-			"parking", "police", "bicycle_parking", 
-			"food_court", "toilets"]
+
 
 for input_path in input_file_path:
 	with open(input_path, 'r', encoding='utf-8') as input_file:
@@ -21,11 +30,14 @@ for input_path in input_file_path:
 		for university in university_list:
 			query = university
 			query_formal_name = query.replace(' ', '_')
-			geojson_features_path = 'map_data/university_map/{query}/{query}_{feature}.geojson'
-			csv_features_path = 'map_data/university_map/{query}/{query}_{feature}.csv'
+			# gm_features_path = 'map_data/university_map_test/{query}/{query}_{feature}.graphml'
+			gpkg_features_path = 'map_data/university_map/{query}/{query}_{feature}.gpkg'
+			gm_features_path = 'map_data/university_map/{query}/{query}_{feature}.graphml'
+			info_file_path = 'map_data/university_map/{query}/{query}_info.json'
 			
+			# 谷歌地图数据获取，获取地点名、地址、评分
 			try:
-				if not os.path.exists(f'map_data/university_map/{query}'):
+				if not os.path.exists(f'map_data/university_map/{query_formal_name}'):
 					os.makedirs(f'map_data/university_map/{query_formal_name}')
 				else:
 					continue
@@ -33,52 +45,48 @@ for input_path in input_file_path:
 				assert(response.status_code == 200)
 				result = response.json()['results'][0]
 				data = {'name': result['name'], 'address': result['formatted_address'], 
-						'formatted_address': result['plus_code']['compound_code'].replace(' ', '_'),
 						'rating': result['rating']}
 				data_with_parent = {'university': data}
-				with open(f'map_data/university_map/{query_formal_name}/{query_formal_name}_info.json', 'w', encoding='gb2312') as f:
+				with open(info_file_path.format(query=query_formal_name), 'w', encoding='gb2312') as f:
 					json.dump(data_with_parent, f, indent=4, ensure_ascii=False)
-					print(f'Basic info of {query} has been found and stored at map_data/university_map/{query}/{query_formal_name}_info.json')
+					print(f'Basic info of {query} has been found and stored at {info_file_path.format(query=query_formal_name)}')
 			except:
 				# time.sleep(1)
 				print(f"{query} does not exits\n")
 				continue
+
+			# 道路网络数据获取，获取选定地点为中心的1200*1200米的bbox内的所有道路数据
 			try:
-				graph = ox.graph_from_place(query)
-				nodes, edges = ox.graph_to_gdfs(graph)
-				nodes.to_csv(csv_features_path.format(query=query_formal_name, feature='nodes'))
-				edges.to_csv(csv_features_path.format(query=query_formal_name, feature='edges'))
-				print(f"Nodes and Edges for {query} have been found and were stored at {geojson_features_path.format(query=query_formal_name, feature='nodes/edges')}")
+				graph = ox.graph_from_address(query, dist=1200)
+				ox.save_graphml(graph, filepath=gm_features_path.format(query=query_formal_name, feature='graph')) # Graph类型数据须存储作.graphml格式，避免坐标系混乱
+				print(f"Nodes and Edges for {query} have been found and were stored at {gpkg_features_path.format(query=query_formal_name, feature='graph')}")
 			except:
 				print(f"Neither node nor edge for {query} was found")
 			
+			# 以下数据类型均存储为.gpkg格式，可保留基本坐标系信息
+			# 获取地点占据区域
 			try:
 				area = ox.geocode_to_gdf(query)
-				area.to_file(geojson_features_path.format(query=query_formal_name, feature='area'), driver='GeoJSON')
-				print(f"Area in {query} has been found and was stored at {geojson_features_path.format(query=query_formal_name, feature='area')}")
+				area.to_file(gpkg_features_path.format(query=query_formal_name, feature='area'), driver='GeoJSON')
+				print(f"Area in {query} has been found and was stored at {gpkg_features_path.format(query=query_formal_name, feature='area')}")
 			except:
 				print(f"No area was found in {query}")
 
+			
 			try:
 				buildings = ox.features_from_place(query, {"building": True})
-				assert(len(buildings) > 0)
-				buildings.to_file(geojson_features_path.format(query=query_formal_name, feature='bulidings'), driver='GeoJSON')
-				print(f"{len(buildings)} buildings in {query} have been found and stored at {geojson_features_path.format(query=query_formal_name, feature='bulidings')}")
+				buildings = buildings.apply(lambda c: c.astype(str) if c.name != "geometry" else c, axis=0)
+				buildings.to_file(f"{gpkg_features_path.format(query=query_formal_name, feature='buildings')}", driver="GPKG")
+				print(f"Buildings in {query} have been found and stored at {gpkg_features_path.format(query=query_formal_name, feature='buildings')}")
 			except:
 				print(f"No building was found in {query}")
-			for amenity in amenities:
-				try:
-					feature = ox.features_from_place(query, {"amenity": amenity})
-					assert(len(feature) > 0)
-					feature.to_file(geojson_features_path.format(query=query_formal_name, feature=amenity), driver='GeoJSON')
-					print(f"{len(feature)} {amenity} in {query} have been found andstored at {geojson_features_path.format(query=query_formal_name, feature=amenity)}")
-				except:
-					print(f"No {amenity} was found in {query}")
+
+			# 获取地点占据区域内设施
 			try:
-				water = ox.features_from_place(query, {"natural": "water"})
-				assert(len(water) > 0)
-				water.to_file(geojson_features_path.format(query=query_formal_name, feature='water'), driver='GeoJSON')
-				print(f"{len(water)} water area in {query} have been found and stored at {geojson_features_path.format(query=query_formal_name, feature='water')}")
+				amenity = ox.features_from_place(query, tags={"amenity": True})
+				amenity = amenity.apply(lambda c: c.astype(str) if c.name != "geometry" else c, axis=0)
+				amenity.to_file(f"{gpkg_features_path.format(query=query_formal_name, feature='amenity')}", driver="GPKG") 
+				print(f"Amenity found in {query}")
 			except:
-				print(f"No water was found in {query}")
+				print(f"No amenity was found in {query}")
 			print("\n")
--- map_data/data_parser/map_parser.py
+++ map_data/data_parser/map_parser.py
@@ -1,13 +1,37 @@
+"""
+地图数据处理模块
+功能: 删除数据文件夹中数据不完整的文件夹, 并从特定格式文件中还原networkx和geopandas等库中规定的数据.
+然后利用matplotlib中的方法对地图进行可视化还原, 这个模块仅作实验性使用, 日后可能其功能会被拆分.
+
+作者: 字禹润
+
+创建日期: 2024-03-21
+最后修改日期: 2024-03-24
+"""
+
+
 import geopandas as gpd
 import os
 import shutil
 import matplotlib.pyplot as plt
 import json
-
+import networkx as nx
+import osmnx as ox
+from shapely.geometry import Polygon, MultiPolygon, Point
+from random import randint
 
 
 
 def list_subdirectories(root_dir):
+    """
+    该函数可以返回文件夹中所有子目录。
+
+    Parameter:
+    root_dir (str): 根目录地址
+
+    Returns:
+    list: 根目录下所有子目录
+    """
     subdirectories = []
     # 遍历root_dir下的所有文件和文件夹
     for item in os.listdir(root_dir):
@@ -20,6 +44,16 @@ def list_subdirectories(root_dir):
 
 
 def delete_folders_with_few_files(root_dir, min_files=2):
+    """
+    该函数可以删除给定根目录下文件个数少于等于min_files个数的子文件夹
+
+    Parameter:
+    root_dir (str): 根目录地址
+    min_files (int): 文件夹删除阈值
+
+    Returns:
+    void
+    """
     for dirpath, dirnames, filenames in os.walk(root_dir, topdown=False):
         if len(filenames) <= min_files and dirpath != root_dir:  # 排除根文件夹
             for file in filenames:
@@ -29,6 +63,15 @@ def delete_folders_with_few_files(root_dir, min_files=2):
 
 
 def get_files_in_folder(folder_path):
+    """
+    该函数可以返回给定目录下所有文件的路径列表
+
+    Parameter:
+    folder_path (str): 文件夹如今
+
+    Returns:
+    list: 目录下的所有文件的路径组成的列表
+    """
     files = []
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
@@ -38,74 +81,186 @@ def get_files_in_folder(folder_path):
 
 
 def info_parser(file):
+    """
+    *_info.json文件的解析函数, 目前只回返回"name"这个key的对应value
+
+    Parameter:
+    file (str): 文件路径
+
+    Returns:
+    str: 地点中文名
+    """
     with open(file, 'r', encoding='gb2312') as f:
         js = json.load(f)
         return js["university"]["name"]
 
 
 def regular_parser(file):
-    return gpd.read_file(file)
+    """
+    常规地理信息文件的解析函数, 返回一个GeoDataFrame.
+
+    Parameter:
+    file (str): 文件路径
+
+    Returns:
+    GeoDataFrame: 读取特定地理信息文件后形成的GeoDataFrame
+    """
+    gdf = gpd.read_file(file)
+    return gdf
+
+
+def graph_parser(file):
+    """
+    graph类型文件的解析函数, 返回一个graph类型的对象.
+
+    Parameter:
+    file (str): 文件路径
+
+    Returns:
+    Graph: networkx库中定义的存储图的数据对象, 可能是Graph, DiGraph, MultiGraph, MultiDiGraph中的一种
+    """
+    return ox.load_graphml(file)
+
+
+def area_parser(file):
+    """
+    Deprecated. 与regular_parser功能相同
+    """
+    gdf = gpd.read_file(file)
+    return gdf
 
 
 def amenity_parser(file):
-    return gpd.read_file(file, GEOM_POSSIBLE_NAMES="geometry", KEEP_GEOM_COLUMNS="NO")
+    """
+    amenity类型文件的解析函数, 会对一个地点范围内的设施进行筛选并返回一个GeoDataFrame. 
+
+    Parameter:
+    file (str): 文件路径
+
+    Returns:
+    GeoDataFrame: 地点范围内设施的data set.
+    """
+    try:
+        gdf = gpd.read_file(file)
+        gdf = gdf[gdf['amenity'] != 'university'].drop_duplicates(subset=['name']) # 将含'university'的数据行删去，并去除名字重复的数据
+        gdf = gdf[gdf['name'] != 'nan'] # 将含'nan'的数据行删去
+        # print(gdf.head())
+        return gdf
+    except:
+        print("Amenity file parsing error")
+        return None
+
+
+def amenity_filter(amenity_gdf, area_gdf):
+    """
+    amenity类型文件的进一步筛选函数, 目前已废弃. 
+    """
+    try:
+        # 定义函数处理每个几何对象
+        def filter_geometry(row):
+            geom = row['geometry']
+            if isinstance(geom, Point) or isinstance(geom, Polygon):
+                return any(geom.within(polygon) for polygon in area_gdf['geometry'])
+            elif isinstance(geom, MultiPolygon):
+                for poly in geom.geoms:
+                    if any(poly.within(polygon) for polygon in area_gdf['geometry']):
+                        return True
+                return False
+
+        # 对每个 'amenity_gdf' 中的几何值进行空间查询，并保留在 'area_gdf' 中的数据
+        filtered_gdf = amenity_gdf[amenity_gdf.apply(filter_geometry, axis=1)]
+        return filtered_gdf
+    except Exception as e:
+        print("filter error: ", e)
+        return amenity_gdf
 
 
 def map_view_generator(map_basket, university):
+    """
+    根据各类数据集合对各种数据进行可视化的函数
+
+    Parameter:
+    map_basket (dict): 特定地点的数据集合
+    university (str): 文件命名所用字符串的组成部分
+
+    Returns:
+    void
+    """
+
+    # 创建thumbnail文件夹，目前该函数仅用于生成缩略图
     if not os.path.exists(f'map_data/map_thumbnail'):
         os.makedirs(f'map_data/map_thumbnail')
-    fig, ax = plt.subplots(figsize=(12, 8))
-    # Plot the footprint
-    if map_basket["area"] is not None:
-        map_basket["area"].plot(ax=ax, facecolor="black")
 
-    # Plot street edges
-    if map_basket["edge"] is not None:
-        map_basket["edge"].plot(ax=ax, linewidth=1, edgecolor="dimgray")
+    if map_basket["graph"] is not None: # 先对存储有road network的graph数据进行可视化，具体参数详见osmnx及matplotlib文档
+        fig, ax = ox.plot_graph(
+            map_basket["graph"],
+            show=False,
+            close=False,
+            bgcolor="#141414",
+            edge_color="lightgrey",
+            edge_linewidth=1,
+            node_size=0,
+        )
 
-    # Plot buildings
-    if map_basket["building"] is not None:
-        map_basket["building"].plot(ax=ax, facecolor="silver", alpha=0.7)
+        # 显示路名
+        for _, edge in ox.graph_to_gdfs(map_basket["graph"], nodes=False).fillna("").iterrows():
+            random_number = randint(1, 100) # 由于每条边都有命名，所以将随机显示，避免文字过密
+            if (random_number % 3 == 0):
+                text = edge["name"]
+                c = edge["geometry"].centroid
+                ax.annotate(text, (c.x, c.y), c="white", fontname='Microsoft YaHei', fontsize=3)
 
-    if map_basket["water"] is not None:
-        map_basket["water"].plot(ax=ax, facecolor="xkcd:sky blue")
+        # 显示地点占据区域
+        if map_basket["area"] is not None:
+            map_basket["area"].plot(ax=ax, facecolor='dimgrey', linewidth=0.6, edgecolor='black', alpha=0.5)
+            offset = 0.001 # offset参数，单位为"°"，用于控制缩放范围
+            bbox = map_basket["area"].total_bounds # 区域占据的bbox大小，用于限定显示范围
+            ax.set_xlim(bbox[0]-offset, bbox[2]+offset)
+            ax.set_ylim(bbox[1]-offset, bbox[3]+offset)
 
-    # Plot restaurants
-    # restaurants.plot(ax=ax, color="yellow", alpha=0.7, markersize=10)
-    plt.tight_layout()
-    file_name = university.split('\\')[1]
-    plt.show()
-    plt.savefig(f'map_data/map_thumbnail/map_thumbnail_{file_name}.png', bbox_inches='tight')
-    plt.close()
+        # 显示建筑
+        if map_basket["building"] is not None:
+            map_basket["building"].plot(ax=ax, facecolor='lightgrey')
+
+        #显示设施
+        if map_basket["amenity"] is not None:
+            filtered_gdf = amenity_filter(map_basket["amenity"], map_basket["area"])
+            # 显示设施名，并用红点标注
+            for idx, row in filtered_gdf.iterrows():
+                centroid = row.geometry.centroid
+                ax.text(centroid.x, centroid.y, row['name'], fontsize=4, fontname='Microsoft YaHei', color='white')
+                ax.scatter(centroid.x, centroid.y, color='red', s=10)
+
+        # 缩略图存储文件名
+        file_name = university.split('\\')[1]
+        # 存储文件
+        plt.savefig(f'map_data/map_thumbnail/map_thumbnail_{file_name}.png', bbox_inches='tight', dpi=300)
+        plt.close()
 
 
-
+# main函数
 root_dir = 'map_data/university_map'
 delete_folders_with_few_files(root_dir)
 university_directories = list_subdirectories(root_dir)
 
 for university in university_directories:
     files_path = get_files_in_folder(university)
-    map_basket = {"name": None, "node": None, "edge": None, "area": None, 
-                  "building": None, "water": None, "amenity": []}
+    map_basket = {"name": None, "graph": None, "area": None, 
+                  "building": None, "amenity": None}
     for file in files_path:   
         parsed_line = file.split('_')
         file_type = parsed_line[-1]
         if file_type == 'info.json':
             map_basket["name"] = info_parser(file)
-        elif file_type == 'nodes.csv':
-            map_basket["node"] = regular_parser(file)
-        elif file_type == 'edges.csv':
-            map_basket["edge"] = regular_parser(file)
-        elif file_type == 'area.csv':
+        elif file_type == 'graph.graphml':
+            map_basket["graph"] = graph_parser(file)
+        elif file_type == 'area.gpkg':
             map_basket["area"] = regular_parser(file)
-        elif file_type == 'buildings.csv':
+        elif file_type == 'buildings.gpkg':
             map_basket["building"] = regular_parser(file)
-        elif file_type == 'water.csv':
-            map_basket["water"] = regular_parser(file)
-        # else:
-            #map_basket["amenity"].append(amenity_parser(file))
-    # print(os.getcwd())
+        elif file_type == 'amenity.gpkg':
+            map_basket["amenity"] = amenity_parser(file)
+
     map_view_generator(map_basket, university)
     print(university + ' thumbnail generated!')
         
